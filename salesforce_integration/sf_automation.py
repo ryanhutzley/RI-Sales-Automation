ba2ae18982e4df9be83407f7cfba050eb9024256
Ryan Hutzley <ryanhutzley@gmail.com>
2022-02-09 11:57:32 -0800

    refactor sheet ingestion

--- salesforce_integration/sf_automation.py
+++ salesforce_integration/sf_automation.py
@@ -4,19 +4,7 @@ from google.oauth2 import service_account
 from dotenv import load_dotenv
 import os
 
-from sf_upsert_methods import upsert_contacts
-
-# Underworked Account - Automated Databees Merge Sheet ID: 1W1BbZTpBSGlg4nW5OZ_13SR2JBFJtlX9J6hjjo_4kIo
-# W/O Email Sheet Name: Matched Contacts w/o Email - To Upsert
-# W/ Email Sheet Name: Matched Contacts w/ Email - To Upsert
-# Account Tags Sheet Name: Accounts to Update
-
-# All Industries - Automated Databees Merge Sheet ID: 1JrWhimitcDUro-N1nqiU8ZrIsxzyiWP2kOgQegdQW9w
-# Full Contact Sheet Name: ???
-
-# Accounts and Contacts w/o LinkedIn URL ID: 1HhrwsJggJr65PqYcjTw5GmW_3NfmCeVXX03sk2SilKM
-# Account Sheet Name: Accounts: Company LinkedIn URL
-# Contacts: Contacts: Contact LinkedIn URL
+from sf_methods import upsertContacts, upsertFormattedContacts, linkedinUpdate, updateAccountTags
 
 load_dotenv("../.env")
 
@@ -26,116 +14,53 @@ SF_TOKEN = os.environ.get("TOKEN")
 
 sf = Salesforce(username=SF_USERNAME, password=SF_PASSWORD, security_token=SF_TOKEN)
 
-############## GOOGLE SHEETS API ##################
-
 SERVICE_ACCOUNT_FILE = 'keys.json'
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
 
 creds = None
 creds = service_account.Credentials.from_service_account_file(SERVICE_ACCOUNT_FILE, scopes=SCOPES)
 
-UNDERWORKED_DATABEES = '1W1BbZTpBSGlg4nW5OZ_13SR2JBFJtlX9J6hjjo_4kIo'
-ALL_INDUSTRIES_DATABEES = '1JrWhimitcDUro-N1nqiU8ZrIsxzyiWP2kOgQegdQW9w'
-NO_LINKEDIN = '1HhrwsJggJr65PqYcjTw5GmW_3NfmCeVXX03sk2SilKM'
-NLP_DATABEES = '1dTr317px33-Qq4beDik0ZeBm7Q0kE3OifjBqtbcaz-A'
-BOUNCED_EMAIL_CONTACTS = '1ed0Tffluu89Xm3Om47Otc5SHR1z8G4j6xNJwZnjPbPg'
-
 service = build('sheets', 'v4', credentials=creds)
 
 sheet = service.spreadsheets()
 
-########### CONTACT UPSERTS ##############
+# NLP_DATABEES = ['1dTr317px33-Qq4beDik0ZeBm7Q0kE3OifjBqtbcaz-A', 'Underworked: Contacts w/ Email - To Upsert!A:J', 'Underworked: Contacts w/o Email - To Upsert!A:I']
+# BOUNCED_EMAIL_CONTACTS = ['1ed0Tffluu89Xm3Om47Otc5SHR1z8G4j6xNJwZnjPbPg', 'Bounced Email Contacts!A:J']
 
-contacts_no_email = sheet.values().get(spreadsheetId=UNDERWORKED_DATABEES, range='Matched Contacts w/o Email - To Upsert!A:F').execute()
-contacts_to_upsert_no_email = contacts_no_email.get('values', [])
-contacts_to_upsert_no_email.pop(0)
+SHEET_IDS = {
+    'UNDERWORKED_DATABEES': ['1W1BbZTpBSGlg4nW5OZ_13SR2JBFJtlX9J6hjjo_4kIo', 'Matched Contacts w/o Email - To Upsert!A:F', 'Matched Contacts w/ Email - To Upsert!A:G', 'Accounts to Update!A:C'],
+    'NEW_NLP_DATABEES_MERGE': ['13owKMVnEIzS5rXKPG8xfqcxKm9C08a4Gumpn5fcDyCc', 'Matched Contacts w/o Email - To Upsert!A:Q', 'Matched Contacts w/ Email - To Upsert!A:R', 'Matched Accounts - To Update!K:M'],
+    'ALL_INDUSTRIES_DATABEES': ['1JrWhimitcDUro-N1nqiU8ZrIsxzyiWP2kOgQegdQW9w', 'Matched Contacts w/o Email - To Upsert!A:Q', 'Matched Contacts w/ Email - To Upsert!A:R'],
+    'NO_LINKEDIN': ['1HhrwsJggJr65PqYcjTw5GmW_3NfmCeVXX03sk2SilKM', 'Accounts: Company LinkedIn URL!A:C', 'Contacts: Contact LinkedIn URL!A:C']
+}
 
-contacts_email = sheet.values().get(spreadsheetId=UNDERWORKED_DATABEES, range='Matched Contacts w/ Email - To Upsert!A:G').execute()
-contacts_to_upsert_email = contacts_email.get('values', [])
-contacts_to_upsert_email.pop(0)
+for key, sheet_info in SHEET_IDS.items():
 
-additional_contacts_no_email = sheet.values().get(spreadsheetId=ALL_INDUSTRIES_DATABEES, range='Matched Contacts w/o Email - To Upsert!A:Q').execute()
-added_contacts_no_email = additional_contacts_no_email.get('values', [])
-added_contacts_no_email.pop(0)
-formatted_added_no_email = [[row[1].strip(), row[6].strip(), row[12].strip(), row[13].strip(), row[15].strip(), row[14].strip()] for row in added_contacts_no_email]
+    sheet_id = sheet_info[0]
 
-additional_contacts_email = sheet.values().get(spreadsheetId=ALL_INDUSTRIES_DATABEES, range='Matched Contacts w/ Email - To Upsert!A:R').execute()
-added_contacts_email = additional_contacts_email.get('values', [])
-added_contacts_email.pop(0)
-formatted_added_email = [[row[1].strip(), row[6].strip(), row[12].strip(), row[13].strip(), row[15].strip(), row[14].strip(), row[16].strip()] for row in added_contacts_email]
-
-# nlp_contacts_email = sheet.values().get(spreadsheetId=NLP_DATABEES, range='Underworked: Contacts w/ Email - To Upsert!A:J').execute()
-# nlp_email = nlp_contacts_email.get('values', [])
-# nlp_email.pop(0)
-# formatted_nlp_email = [[row[0].strip(), row[3].strip(), row[4].strip(), row[5].strip(), row[7].strip(), row[6].strip(), row[8].strip()] for row in nlp_email]
-
-nlp_contacts_no_email = sheet.values().get(spreadsheetId=NLP_DATABEES, range='Underworked: Contacts w/o Email - To Upsert!A:I').execute()
-nlp_no_email = nlp_contacts_no_email.get('values', [])
-nlp_no_email.pop(0)
-formatted_nlp_no_email = [[row[0].strip(), row[3].strip(), row[4].strip(), row[5].strip(), row[7].strip(), row[6].strip()] for row in nlp_no_email]
-
-############ COMPANY/CONTACT LINKEDIN UPDATES #############
-
-company_linkedinURLs = sheet.values().get(spreadsheetId=NO_LINKEDIN, range='Accounts: Company LinkedIn URL!A:C').execute()
-company_URLs = company_linkedinURLs.get('values', [])
-company_URLs.pop(0)
-formatted_company_URLs = [{'Id': row[0], 'Company_LinkedIn_URL__c': row[1]} for row in company_URLs]
-
-contact_linkedinURLs = sheet.values().get(spreadsheetId=NO_LINKEDIN, range='Contacts: Contact LinkedIn URL!A:C').execute()
-contact_URLs = contact_linkedinURLs.get('values', [])
-contact_URLs.pop(0)
-formatted_contact_URLs = [{'Id': row[0], 'Contact_LinkedIn_URL__c': row[1]} for row in contact_URLs]
-
-################# ACCOUNT TAGS UPDATES ####################
-
-accounts_with_tags = sheet.values().get(spreadsheetId=UNDERWORKED_DATABEES, range='Accounts to Update!A:C').execute()
-accounts_with_matching_tags = accounts_with_tags.get('values', [])
-accounts_with_matching_tags.pop(0)
-formatted_accounts_with_matching_tags = [{'Id': row[0], 'Account_Hashtag__c': row[1]} for row in accounts_with_matching_tags]
-
-# nlp_account_tags_update = sheet.values().get(spreadsheetId=NLP_DATABEES, range='Underworked: Accounts to Update!A:C').execute()
-# nlp_tags_update = nlp_account_tags_update.get('values', [])
-# nlp_tags_update.pop(0)
-# formatted_nlp_tags_update = [{'Id': row[0], 'Account_Hashtag__c': row[1]} for row in nlp_tags_update]
-# sf.bulk.Account.update(formatted_nlp_tags_update, batch_size=10000, use_serial=False)
-# print('Done with NLP Account tag updates')
-
-############# BOUNCED EMAIL CLEANSING ################## 
-
-# bounced_contacts_from_sf = sheet.values().get(spreadsheetId=BOUNCED_EMAIL_CONTACTS, range='Bounced Email Contacts!A:J').execute()
-# bounced_contacts_sf = bounced_contacts_from_sf.get('values', [])
-# bounced_contacts_sf.pop(0)
-# formatted_bounced_contacts_sf = [{'Id': row[0], 'Email': '', 'Contact_Notes__c': f'Bounced Email: {row[1]}'} for row in bounced_contacts_sf]
-
-# databees_sheet_update = sheet.values().append(spreadsheetId=BOUNCED_EMAIL_CONTACTS, range='Bounced Email Contacts Databees', valueInputOption='USER_ENTERED', body={'values': bounced_contacts_sf}).execute()
-
-# sf.bulk.Contact.update(formatted_bounced_contacts_sf, batch_size=10000, use_serial=False)
-
-############ UPDATE FUNCTION INVOCATIONS #############
-
-upsert_contacts(sf, contacts_to_upsert_no_email)
-print('Done with contacts_to_upsert_no_email')
-
-upsert_contacts(sf, contacts_to_upsert_email)
-print('Done with contacts_to_upsert_email')
-
-upsert_contacts(sf, formatted_added_no_email)
-print('Done with formatted_added_no_email')
-
-upsert_contacts(sf, formatted_added_email)
-print('Done with formatted_added_email')
-
-# upsert_contacts(sf, formatted_nlp_email)
-# print('Done with formatted_nlp_email')
-
-upsert_contacts(sf, formatted_nlp_no_email)
-print('Done with formatted_nlp_no_email')
-
-sf.bulk.Account.update(formatted_company_URLs, batch_size=10000, use_serial=False)
-print('Done with Account LinkedInURL updates')
-
-sf.bulk.Contact.update(formatted_contact_URLs, batch_size=10000, use_serial=False)
-print('Done with Contact LinkedInURL updates')
-
-sf.bulk.Account.update(formatted_accounts_with_matching_tags, batch_size=10000, use_serial=False)
-print('Done with Account tag updates')
+    if key == 'NO_LINKEDIN':
+        company_urls = sheet.values().get(spreadsheetId=sheet_id, range=sheet_info[1]).execute().get('values', [])
+        linkedinUpdate(False, company_urls, sf)
+        print('Done with Company LinkedIn Updates')
+        contact_urls = sheet.values().get(spreadsheetId=sheet_id, range=sheet_info[2]).execute().get('values', [])
+        linkedinUpdate(True, contact_urls, sf)
+        print('Done with Contact LinkedIn Updates')
+    else:
+        no_email = sheet.values().get(spreadsheetId=sheet_id, range=sheet_info[1]).execute().get('values', [])
+        email = sheet.values().get(spreadsheetId=sheet_id, range=sheet_info[2]).execute().get('values', [])
+        if key == 'UNDERWORKED_DATABEES':
+            no_email.pop(0)
+            upsertContacts(sf, no_email)
+            print(f'Done with {key} no email upserts')
+            email.pop(0)
+            upsertContacts(sf, email)
+            print(f'Done with {key} email upserts')
+        else:
+            upsertFormattedContacts(False, no_email, sf)
+            print(f'Done with {key} no email upserts')
+            upsertFormattedContacts(True, email, sf)
+            print(f'Done with {key} email upserts')
+        if len(sheet_info) > 3:
+            account_tags = sheet.values().get(spreadsheetId=sheet_id, range=sheet_info[3]).execute().get('values', [])
+            updateAccountTags(account_tags, sf)
+            print(f'Done with {key} account tag updates')
---
+++ salesforce_integration/sf_methods.py
@@ -0,0 +1,79 @@
+from simple_salesforce.exceptions import SalesforceMalformedRequest, SalesforceResourceNotFound
+
+def upsertContacts(sf, contacts):
+
+    for row in contacts:
+
+        account_id = row[0].strip()
+        company_linkedin = row[1].strip()
+        first_name = row[2].strip()
+        last_name = row[3].strip()
+        contact_linkedin = row[4].strip()
+        title = row[5].strip()
+        email = ''
+        try:
+            email = row[6].strip()
+        except:
+            email = ''
+
+        if account_id == '':
+            print('No more contacts')
+            break
+
+        try:
+            sf.Contact.create(
+                {
+                    'AccountId': f'{account_id}',
+                    'Company_LinkedIn_URL__c': f'{company_linkedin}',
+                    'FirstName': f'{first_name}',
+                    'LastName': f'{last_name}', 
+                    'Contact_LinkedIn_URL__c': f'{contact_linkedin}', 
+                    'Title': f'{title}',
+                    'Email': f'{email}'
+                }
+            )
+            print(f'{account_id} {first_name} {last_name} created')
+        except SalesforceMalformedRequest as e:
+            contact_id = e.content[0]['duplicateResut']['matchResults'][0]['matchRecords'][0]['record']['Id']
+            try:
+                sf.Contact.update(
+                    f'{contact_id}', 
+                    {
+                        'Company_LinkedIn_URL__c': f'{company_linkedin}', 
+                        'Contact_LinkedIn_URL__c': f'{contact_linkedin}', 
+                        'Title': f'{title}',
+                        'Email': f'{email}'
+                    }
+                )
+                print(f'{contact_id} {first_name} {last_name} updated')
+            except SalesforceResourceNotFound as e:
+                print(f'COULD NOT BE UPDATED: {contact_id} {first_name} {last_name}')
+
+
+def upsertFormattedContacts(is_email, data, sf):
+
+    data.pop(0)
+    if is_email: 
+        formatted_data_email = [[row[1].strip(), row[6].strip(), row[12].strip(), row[13].strip(), row[15].strip(), row[14].strip(), row[16].strip()] for row in data]
+        upsertContacts(sf, formatted_data_email)
+    else:
+        formatted_data_no_email = [[row[1].strip(), row[6].strip(), row[12].strip(), row[13].strip(), row[15].strip(), row[14].strip()] for row in data]
+        upsertContacts(sf, formatted_data_no_email)
+
+
+def linkedinUpdate(is_contact, data, sf):
+    
+    data.pop(0)
+    if is_contact:
+        formatted_contact_linkedins = [{'Id': row[0], 'Contact_LinkedIn_URL__c': row[1]} for row in data]
+        sf.bulk.Contact.update(formatted_contact_linkedins, batch_size=10000, use_serial=False)
+    else:
+        formatted_company_linkedins = [{'Id': row[0], 'Company_LinkedIn_URL__c': row[1]} for row in data]
+        sf.bulk.Account.update(formatted_company_linkedins, batch_size=10000, use_serial=False)
+    
+
+def updateAccountTags(data, sf):
+
+    data.pop(0)
+    tags = [{'Id': row[0], 'Account_Hashtag__c': row[1]} for row in data]
+    sf.bulk.Account.update(tags, batch_size=10000, use_serial=False)
