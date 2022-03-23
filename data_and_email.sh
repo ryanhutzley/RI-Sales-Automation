f3d8d8e7d2fa6cfe96c4454ba1fdd294f585ff55
Ryan Hutzley <ryanhutzley@gmail.com>
2022-03-22 17:05:39 -0700

    shell script for cron job

---
+++ data_and_email.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+source /Users/ryanhutzley/.bash_profile && conda deactivate && conda activate ri-sales && python3 /Users/ryanhutzley/Desktop/RI\ Sales\ Automation/data_and_emails/ingester_and_emailer.py
